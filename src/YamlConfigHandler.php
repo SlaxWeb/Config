bf908e39e2c9c90c3d2f9ec07b22020b12e2c25c
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-02-13 18:29:00 +0100

    add the interface and implement in abstract class

---
+++ src/YamlConfigHandler.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Yaml Config Handler
+ *
+ * Handles loading of Yaml config files, and parsing their contents into the
+ * config array.
+ *
+ * @package   SlaxWeb\Config
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+namespace SlaxWeb;
+
+class YamlConfigHandler extends ConfigHandler
+{
+    /**
+     * Load the Config File
+     *
+     * Check if the file exists, load it, and parse the containing array into
+     * the the internal container array. Return 'CONFIG_LOADED' constant on
+     * success. If the file is not found, return 'CONFIG_RESOURCE_NOT_FOUND'
+     * constant, and 'CONFIG_PARSE_ERROR' if the contants could not have been
+     * parsed.
+     *
+     * @param string $config Path to the config resource
+     * @return int
+     */
+    public function load($config)
+    {
+        // check file exists
+        if (file_exists($config) === false) {
+            return static::CONFIG_RESOURCE_NOT_FOUND;
+        }
+
+        require_once $config;
+
+        if (isset($configuration) === false) {
+            return static::CONFIG_PARSE_ERROR;
+        }
+
+        $this->_configValues = array_merge($this->_configValues, $configuration);
+        return static::CONFIG_LOADED;
+    }
+}
