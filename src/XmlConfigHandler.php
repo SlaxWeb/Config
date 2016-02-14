8bd095abb2857ffc1ac512222a7f3f834e3da0f4
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-02-14 15:09:55 +0100

    add XML config handler

---
+++ src/XmlConfigHandler.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * XML Config Handler
+ *
+ * Handles loading of XML config files, and parsing their contents into the
+ * config array.
+ *
+ * @package   SlaxWeb\Config
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+namespace SlaxWeb;
+
+class XmlConfigHandler extends ConfigHandler
+{
+    /**
+     * XML Library
+     *
+     * @var Desperado\XmlBundle\Model\XmlReader
+     */
+    protected $_xml = null;
+
+    /**
+     * XmlConfigHandler constructor
+     *
+     * Set the Desperado\XmlBundle\Model\XmlReader to the protected property for use later.
+     *
+     * return void
+     */
+    public function __construct(\Desperado\XmlBundle\Model\XmlReader $xml)
+    {
+        $this->_xml = $xml;
+    }
+
+    /**
+     * Load the Config File
+     *
+     * Check if the file exists, load it, and parse the containing array into
+     * the the internal container array. Return 'CONFIG_LOADED' constant on
+     * success. If the file is not found, return 'CONFIG_RESOURCE_NOT_FOUND'
+     * constant, and 'CONFIG_PARSE_ERROR' if the contants could not have been
+     * parsed.
+     *
+     * @param string $config Path to the config resource
+     * @return int
+     */
+    public function load($config)
+    {
+        // check file exists
+        if (file_exists($config) === false) {
+            return static::CONFIG_RESOURCE_NOT_FOUND;
+        }
+
+        $configContents = file_get_contents($config);
+        if (($configuration = $this->_xml->processConvert($configContents)) === []) {
+            return static::CONFIG_PARSE_ERROR;
+        }
+
+        $this->_configValues = array_merge($this->_configValues, $configuration);
+        return static::CONFIG_LOADED;
+    }
+}
---
+++ tests/XmlConfigHandlerTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * YAML Config Handler Test
+ *
+ * Test class for the YAML Config Handler class of the SlaxWeb\Config component.
+ *
+ * @package   SlaxWeb\Config
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+namespace SlaxWeb\Config\Tests;
+
+use SlaxWeb\XmlConfigHandler as ConfigHandler;
+
+class XmlConfigHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $handler = $this->getMockBuilder("\\SlaxWeb\\XmlConfigHandler")
+            ->disableOriginalConstructor()
+            ->setMethods(null)
+            ->getMock();
+
+        $xmlParser = $this->getMockBuilder("\\Desperado\\XmlBundle\\Model\\XmlReader")
+            ->setMethods(["processConvert"])
+            ->getMock();
+
+        $handler->__construct($xmlParser);
+    }
+
+    /**
+     * Test 'load' method
+     *
+     * This test ensures that the 'load' method functions according to
+     * specification. The 'load' method should receive full absolute path to the
+     * config file it must load, and forward the contents to the '_parse'
+     * protected method.
+     *
+     * If the file it should load does not exist, it must return the 'file not
+     * found' constant, or if the contents of the file could not have been
+     * parsed it must return the 'parse error' constant. If everything went as
+     * expected it must return the 'loaded' constants.
+     */
+    public function testLoad()
+    {
+        $handler = $this->getMockBuilder("\\SlaxWeb\\XmlConfigHandler")
+            ->disableOriginalConstructor()
+            ->setMethods(null)
+            ->getMock();
+
+        $xmlParser = $this->getMockBuilder("\\Desperado\\XmlBundle\\Model\\XmlReader")
+            ->setMethods(["processConvert"])
+            ->getMock();
+
+        $xmlParser->expects($this->exactly(2))
+            ->method("processConvert")
+            ->will($this->onConsecutiveCalls(
+                ["test.config" => "test"],
+                []
+            ));
+        $handler->__construct($xmlParser);
+
+        // file found, and parsed
+        $this->assertEquals(
+            $handler->load(__DIR__ . "/TestConfig/XmlConfig.xml"),
+            ConfigHandler::CONFIG_LOADED
+        );
+        // file not found
+        $this->assertEquals(
+            $handler->load(__DIR__ . "/TestConfig/NotFound.xml"),
+            ConfigHandler::CONFIG_RESOURCE_NOT_FOUND
+        );
+        // file found, parsing failed
+        $this->assertEquals(
+            $handler->load(__DIR__ . "/TestConfig/NotXmlConfig.xml"),
+            ConfigHandler::CONFIG_PARSE_ERROR
+        );
+
+        $this->assertEquals($handler->get("test.config"), "test");
+    }
+}
