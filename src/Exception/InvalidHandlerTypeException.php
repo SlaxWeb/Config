c33288c6ce0eaa4f95c95f1eebee2eee5bf8f541
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-02-15 20:40:32 +0100

    add a factory class for easier instantiation

---
+++ src/Exception/InvalidHandlerTypeException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Invalid Handler Type Exception
+ *
+ * Thrown in the Factory class, if an invalid handler is requested to be
+ * injected to the Config class.
+ *
+ * @package   SlaxWeb\Config
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+namespace SlaxWeb\Config\Exception;
+
+class InvalidHandlerTypeException extends \Exception
+{
+}
---
+++ src/Factory.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Config Component Factory
+ *
+ * The Factory takes care of all the Config class dependencies, instantiates
+ * it, and returns its object.
+ *
+ * @package   SlaxWeb\Config
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+namespace SlaxWeb\Config;
+
+class Factory
+{
+    /**
+     * Instantiate Config class
+     *
+     * Instantiate the configuration handler, pass it to the Config class when
+     * instantiating it, and return its object.
+     * @param string $handlerClass Type of handler to use
+     * @param string $resLocation Location of configuration resource
+     * @return \SlaxWeb\Config\Config
+     */
+    public static function init(string $handlerType, string $resLocation): Config
+    {
+        $handler = null;
+        switch (strtolower($handlerType)) {
+            case "php":
+                $handler = new PhpConfigHandler;
+                break;
+            case "xml":
+                $xml = new \Desperado\XmlBundle\Model\XmlReader;
+                $handler = new XmlConfigHandler($xml);
+                break;
+            case "yaml":
+                $handler = new YamlParserMissingException;
+                break;
+            default:
+                throw new Exception\InvalidHandlerTypeException(
+                    "Handler type must be one of ['php', 'xml', 'yaml']"
+                );
+        }
+
+        return new Config($handler, $resLocation);
+    }
+}
