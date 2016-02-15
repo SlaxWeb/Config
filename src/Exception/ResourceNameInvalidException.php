a6c6e66d345963b5555acdac868a89fba55813b0
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-02-15 20:28:18 +0100

    add 'load' method to config class and test

---
+++ src/Exception/ResourceNameInvalidException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Resource Name Invalid Exception
+ *
+ * Thrown when the received resource name for loading is not valid.
+ *
+ * @package   SlaxWeb\Config
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.1
+ */
+namespace SlaxWeb\Config\Exception;
+
+class ResourceNameInvalidException extends \Exception
+{
+}
